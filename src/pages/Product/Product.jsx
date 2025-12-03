1afe04b7efe702c87e56636afa4fd9f420b2d530
Lucik <kseniatopol54@gmail.com>
2025-12-03 22:39:32 +0500

    Product.jsx

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { books } from './../../data/books';
 import './Product.css';
 
 function Product() {
+  const {id} = useParams();
+  const product = books.find(book => book.id === parseInt(id));
+
+  if(!product) {
+    return <div>Товар не найден</div>
+  }
   
   return (
-    <div>
+    <div className='product-card'>
+      <img src={product.book_img} alt={product.title} className='product-image'/>
+      <h2 className="product-title">{product.title}</h2>
+      <p className="product-author"><strong>Автор:</strong> {product.author}</p>
+      <p className="product-genre"><strong>Жанр:</strong> {product.genre}</p>
+      <p className="product-description">{product.description}</p>
+      <p className="product-price">Цена: {product.price} ₽</p>
+      <div className="product-rating">
+        <strong>Рейтинг:</strong> {product.rating} ⭐️ ({product.reviews} отзывов)
+      </div>
     </div>
   );
 }
