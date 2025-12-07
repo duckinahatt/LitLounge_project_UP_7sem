9cbf4eb3d1e9355654023155a49bf1ef2231e00d
duckinahatt <margaritaborodovskih@gmail.com>
2025-12-08 00:41:59 +0500

    Update Header.jsx
    Исправлен путь к изображению логотипа сайта

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
   return (
     <header>
       <NavLink to="/" id="logo" end>
-        <img src="./assets/img/icon_logo.svg" alt="logo" />
+        <img src="./../assets/img/icon_logo.svg" alt="logo" />
         <h1>LitLounge</h1>
       </NavLink>
       <nav>
